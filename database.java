34ffe16aad0aae4c77f154556a84c394ad2ee098
Sasha <avlasova@caroolu.edu>
2025-10-10 16:35:48 -0500

    minor change

--- database.java
+++ database.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 
 public class database<T> {
+    //
     private ArrayCollection<T> allMarkers = new ArrayCollection<T>();
     
     public database(){
